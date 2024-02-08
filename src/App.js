86942acc646bfbff359b41c628e7c6349f41ea20
Austine Mwangi <austinemwangideveloper@gmail.com>
2024-02-08 19:21:52 +0300

    ddd

--- src/App.js
+++ src/App.js
@@ -1,40 +1,109 @@
-import { useState } from "react";
-import PricingCard from "./component/PricingCard";
-import "./styles/PricingApp.css"
+import { useState } from 'react';
+import PricingCard from './component/PricingCard';
+import './styles/PricingApp.css';
 
 function App() {
- const [selectMonthly, setSelectMonthly] = useState(true);
-let Starter=["We will give you this", "We will gijadhsjaehk"]
-let Pro=["We will", "We will "]
-let Enterprise=["We will give you this", "We will gijadhsjaehk"]
+  const [selectMonthly, setSelectMonthly] = useState(true);
+  const [starterList, setStarterList] = useState(true);
+  const [proList, setProList] = useState(true);
+  const [enterpriseList, setEnterpriseList] = useState(true);
+
+  const starterMonthlyList = [
+    'Unlimited Monthly  Bandwidth',
+    'Unlimited Monthly Disk',
+    'Unlimited Monthly Domain',
+    'Unlimited  MonthlyEmail',
+  ];
+  const starterAnnuallyList = [
+    'Unlimited Annual Bandwidth',
+    'Unlimited Annual Disk',
+    'Unlimited Annual Domain',
+    'Unlimited Annual',
+  ];
+  const proListMonthly = [
+    'Unlimited Monthly Bandwidth',
+    'Unlimited MonthlyDisk',
+    'Unlimited MonthlyDomain',
+    'UnlimitedMonthly Email',
+  ];
+  const proListAnnually = [
+    'Unlimited Annual Bandwidth',
+    'Unlimited Annual Disk',
+    'Unlimited  Annual Domain',
+    'Unlimited  AnnualEmail',
+  ];
+  const enterpriseListMonthly = [
+    'Unlimited Monthly Bandwidth',
+    'Unlimited  MonthlyDisk',
+    'Unlimited Monthly Domain',
+    'Unlimited Monthly Email',
+  ];
+  const enterpriseListAnnually = [
+    'Unlimited Annual Bandwidth',
+    'Unlimited Annual Disk',
+    'Unlimited Annual Domain',
+    'Unlimited Annual Email',
+  ];
+  const getStarterList = () => {
+    if (selectMonthly) {
+      return starterMonthlyList;
+    } else {
+      return starterAnnuallyList;
+    }
+  };
+  const getProList = () => {
+    if (selectMonthly) {
+      return proListMonthly;
+    } else {
+      return proListAnnually;
+    }
+  };
+  const getEnterpriseList = () => {
+    if (selectMonthly) {
+      return enterpriseListMonthly;
+    } else {
+      return enterpriseListAnnually;
+    }
+  };
+
   return (
-    <div className="PricingApp">
-      <div className="App-container">
+    <div className='PricingApp'>
+      <div className='App-container'>
         <header>
-          <h1 className="header-topic">Choose A Plan</h1>
-          <div className="header-row">
+          <h1 className='header-topic'>Choose A Plan</h1>
+          <div className='header-row'>
             <p>Billed Annually</p>
-            <label className="price-switch">
-            <input className="price-checkbox" 
-            onChange={() =>{
-              setSelectMonthly((prev) => !prev);
-            }}
-            type="checkbox"  />
-            <div className="switch-slider"></div>
+            <label className='price-switch'>
+              <input
+                className='price-checkbox'
+                onChange={() => {
+                  setSelectMonthly((prev) => !prev);
+                }}
+                type='checkbox'
+              />
+              <div className='switch-slider'></div>
             </label>
             <p>Billed Monthly</p>
           </div>
         </header>
-        
-        <div className="pricing-cards">
-          <PricingCard title="starter"
-          price={selectMonthly ? "$ 19" : "$ 200"} list={}/>
-          <PricingCard title="pro"
-          price={selectMonthly ? "$ 50" : "$ 500"}/>
-          <PricingCard title="Enterprise"
-          price={selectMonthly ? "$ 100" : "$ 800"}/>
-        </div>
 
+        <div className='pricing-cards'>
+          <PricingCard
+            title='starter'
+            price={selectMonthly ? '$ 19' : '$ 200'}
+            features={getStarterList()}
+          />
+          <PricingCard
+            title='pro'
+            price={selectMonthly ? '$ 50' : '$ 500'}
+            features={getProList()}
+          />
+          <PricingCard
+            title='Enterprise'
+            price={selectMonthly ? '$ 100' : '$ 800'}
+            features={getEnterpriseList()}
+          />
+        </div>
       </div>
     </div>
   );
--- src/component/PricingCard.js
+++ src/component/PricingCard.js
@@ -1,23 +1,27 @@
-import React from 'react'
-import "../styles/PricingCard.css";
+import React from 'react';
+import '../styles/PricingCard.css';
 
-const PricingCard = ({title,price,features}) => {
+const PricingCard = ({ title, price, features }) => {
   return (
-    <div className='PricingCard'>
-        <header>
-            <p className='card-title'>{title}</p>
-            <h1 className='card-price'>{price}</h1>
-            
+    <div className='PricingCard' style={{ margin: '20px' }}>
+      <header>
+        <p className='card-title'>{title}</p>
+        <h1 className='card-price'>{price}</h1>
+      </header>
 
-        </header>
-        <div className='card-features'>
-                <div className='features'>{features} </div>
-                
-                
+      <div className='card-features'>
+        {features.map((feature, index) => {
+          return (
+            <div key={index} className='feature'>
+              {feature}
             </div>
-            <button className='card-btn'>Choose plan</button>
-    </div>
-  )
-}
+          );
+        })}{' '}
+      </div>
 
-export default PricingCard
+      <button className='card-btn'>Choose plan</button>
+    </div>
+  );
+};
+
+export default PricingCard;
